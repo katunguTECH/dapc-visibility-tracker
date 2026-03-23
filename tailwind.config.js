ae8d78941837d5810508785e7c301fcf4e309574
katunguTECH <katungu1@gmail.com>
2026-03-23 20:30:20 +0300

    fix: tailwind content paths for Vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,14 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",       // app folder
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",     // pages folder
+    "./components/**/*.{js,ts,jsx,tsx,mdx}", // components folder
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
 export default config;
